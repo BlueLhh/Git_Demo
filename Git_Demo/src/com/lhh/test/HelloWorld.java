bd97d76c63984c6dc207cc0753534fbbcd9da1f1
BlueLhh <lanhonghui100@gmail.com>
2017-08-05 11:20:09 +0800

    第一次上传码云

--- Git_Demo/src/com/lhh/test/HelloWorld.java
+++ Git_Demo/src/com/lhh/test/HelloWorld.java
@@ -3,5 +3,9 @@ package com.lhh.test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello JAVA!");
+		System.out.println("这是第一次上传代码到码云");
 	}
+	
+	
 }
