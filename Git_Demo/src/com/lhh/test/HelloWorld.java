db5618f135045ff435bdecc13be39e3a07ece22b
BlueLhh <lanhonghui100@gmail.com>
2017-08-05 11:29:49 +0800

    第二次上传到Github测试

--- Git_Demo/src/com/lhh/test/HelloWorld.java
+++ Git_Demo/src/com/lhh/test/HelloWorld.java
@@ -4,8 +4,5 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello JAVA!");
-		System.out.println("这是第一次上传代码到码云");
 	}
-	
-	
 }
